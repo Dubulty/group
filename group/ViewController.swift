6025193490f93cf16c61cf5b596a8ab87f997517
Dubulty <118088466+Dubulty@users.noreply.github.com>
2024-01-24 21:05:43 +0400

    Initial commit

---
+++ group/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  group
+//
+//  Created by Андрей Коновалов on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
